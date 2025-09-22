00d1fb019fc6b185ac3ca31aed196cd39b2fd9a8
unomi <taokunkun1@icloud.com>
2025-09-22 08:44:51 +0800

    auto-commit

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.sky.mapper;
+
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+
+
+
+
+
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+}
